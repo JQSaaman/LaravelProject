2c65216d3e1ff2d15110692b07441a356eac4452
JustinSaaman <58013729+JustinSaaman@users.noreply.github.com>
2021-06-09 14:31:35 +0200

    d

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::resource('/posts', PostController::class);
+Route::resource('/posts', PostsController::class);
 
 require __DIR__.'/auth.php';
